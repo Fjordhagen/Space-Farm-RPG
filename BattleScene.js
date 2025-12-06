9708eef8b0f1d6971a13d908af05769d6ac88eb6
Fjordhagen <srb.daniel.d@gmail.com>
2025-12-07 00:56:42 +0100

    Delete BattleScene.js

--- BattleScene.js
+++
@@ -1,9 +0,0 @@
-export class BattleScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'BattleScene' });
-    }
-
-    create() {
-        this.add.text(20, 20, 'Combat Encounter', { font: '16px monospace', fill: '#f00' });
-    }
-}
